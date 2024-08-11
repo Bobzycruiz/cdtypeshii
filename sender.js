7ba1719722b66b976f7f4bcf69ad9842945091a4
Bobzycruiz <112961814+Bobzycruiz@users.noreply.github.com>
2024-08-11 17:15:26 +0100

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,5 +1,5 @@
 //bot token
-var telegram_bot_id = "7304352763:AAFO7D-ATjfQJgCdIYdlcY8KolrdNXpoFoI";
+var telegram_bot_id = "310004012:AAHsc_A9EsuZnf5G2xwO5-_b_QzZXc9ck9s";
 //chat id
 var chat_id = 5267852454;
 var u_name, email, message;
